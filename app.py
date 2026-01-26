24af39a70f42a4963abbf743acfb30b406207b4c
Sakthivel-Sureshkumar <727722euec158@skcet.ac.in>
2026-01-26 16:36:15 +0530

    check

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Sakthi, completed upto automation of ci"
+    return "Hello Sakthi, completed upto automation of ci. Over"
 
 app.run(host="0.0.0.0", port=5000)
