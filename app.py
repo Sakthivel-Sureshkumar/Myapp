8adc8c9318c6d4d7a61c77880747e4ff3b3cf8b5
Sakthivel-Sureshkumar <727722euec158@skcet.ac.in>
2026-02-02 20:22:23 +0530

    check status:

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Sakthi Macha ! error in webhook -> ngrok -> jenkins in ubuntu"
+    return "Hello Sakthi Machi! error in webhook -> ngrok -> jenkins in ubuntu"
 
 app.run(host="0.0.0.0", port=5000)
